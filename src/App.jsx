138de8776341bfd0f8528bd12bd3f0b87a1bb4ec
Jake-Zaruba <94652629+Jake-Zaruba@users.noreply.github.com>
2023-05-02 00:31:59 -0500

    First attempt at adding a reducer function
    Broken but getting there

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer, useState } from "react";
+
+const ACTIONS = { ADD_TODO: "ADD_TODO" };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return { todos: [...state.todos, { text: action.todo }] };
+    default:
+      return state;
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(reducer, { todos: [] });
+  const [todo, setTodo] = useState("");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Todo</h1>
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button onClick={() => dispatch({ type: ACTIONS.ADD_TODO, todo })}>
+        Add todo
+      </button>
+      <button>Delete todo</button>
+      <h2>{state.todos.text}</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
